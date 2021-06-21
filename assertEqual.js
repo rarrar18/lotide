c902802bf43b47c30b32f84e80c1730d4b4dcde1
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-21 21:50:39 +0000

    Added custom console.assert function in assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+//This custom assert function logs a message to the console
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); //comparing non-identical strings
+assertEqual("apple", "apple"); //comparing identical strings
+assertEqual(50, 100); //comparing non-identical numbers
+assertEqual(1, 1); //comparing identical numbers
+assertEqual(true, false); //comparing non-identical values
