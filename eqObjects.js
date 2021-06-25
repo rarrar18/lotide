372bab877489095010289a5e147e4b96d4adfae9
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-25 05:36:27 +0000

    Refactored by creating variables to store key values

--- eqObjects.js
+++ eqObjects.js
@@ -29,25 +29,27 @@ const eqArrays = function(actual, expected) {
 
 // FUNCTION: Takes in two objects and returns true or false, based on a perfect match of keys and values
 const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+  const keys1 = Object.keys(object1); // Returns array of the object 1's keys
+  const keys2 = Object.keys(object2); // Returns array of the object 2's keys
 
-  //compare lengths of two keys
+  // compare lengths of two keys
   if (keys1.length !== keys2.length) {
     return false;
   }
 
   // iterate through array of keys
   for (const key of keys1) {
-    if (Array.isArray(object1[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
+    const key1 = object1[key]; // returns each element of keys1
+    const key2 = object2[key]; // returns each element of keys2
+    if (Array.isArray(key1)) {
+      if (!eqArrays(key1, key2)) { // if elements do not match
         return false;
       }
       continue;
     }
 
     // if the above condition is false
-    if (object1[key] !== object2[key]) {
+    if (key1 !== key2) {
       return false;
     }
   }
