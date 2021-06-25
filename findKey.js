bac1768aff7ec904d95e060ceeb28e0ee7a6ada0
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-25 06:04:17 +0000

    Implemented findKey.js to lotide

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+
+const findKey = function(obj, cb) {
+  for (let key in obj) {
+    if (cb) {
+      console.log(cb);
+      return key;
+    }
+  }
+  return undefined;
+}
+
+// TEST CODE
+
+test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(test);
+
