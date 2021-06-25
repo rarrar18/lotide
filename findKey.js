5215b28f743bca2972daf2d97eb4a44b6c05e823
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-25 20:02:00 +0000

    Added findKey.js that returns the first key with a cb as a truthy value

--- findKey.js
+++ findKey.js
@@ -1,3 +1,37 @@
+// FUNCTION: Takes in object and a callback, scans the object for first key with cb as truthy
+const findKey = function(obj, cb) {
+  // iterate through the keys in the object
+  for (let key in obj) {
+    // for an anon func, make sure you pass in the parameter as well
+    // (in this case obj[key] checks for the value of stars, which is a number)
+    if (cb(obj[key])) {
+      // return the first key where the cb returns a truthy value
+      return key;
+    }
+  }
+  // if no key is found, return undefined
+  return undefined;
+};
+
+// TEST CODE
+const test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+console.log(test); // Prints out the returned value
+
+// FUNCTION: This is the defined version of x => x.stars === 2
+// const cb = (x) => {
+//   // check if this is true, returns either true or false
+//   return x.stars === 2;
+// };
+
+// FUNCTION: Checks if two values are equal
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -5,28 +39,7 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-//Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
 
-const findKey = function(obj, cb) {
-  for (let key in obj) {
-    if (cb) {
-      console.log(cb);
-      return key;
-    }
-  }
-  return undefined;
-}
-
-// TEST CODE
-
-test = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
-
-console.log(test);
-
+// TEST CODE: assertEquals()
+assertEqual(test, 'noma'); // Output: ✅✅✅ Assertion Passed: noma === noma
+assertEqual(test, 'Ora'); // Output: 🛑🛑🛑 Assertion Failed: noma !== Ora
