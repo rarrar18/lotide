c3cc95cb2c6248c4ba6f6771241bdedbc45d55c4
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-29 02:39:48 +0000

    Added tailTest.js which contains test code for tail.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,6 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
-
+/*
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
@@ -12,3 +12,28 @@ tail(single);
 const empty = [];
 tail(empty);
 assertEqual(single.length, empty);
+*/
+
+describe ("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it ("returns tail end of ['Yo Yo', 'Lighthouse', 'Labs'] array", () => {
+    tail(words);
+  });
+  it ("returns 3 for words.length of ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(words.length, 3);
+  });
+  // it ("returns tail end of [1]", () => {
+  //   tail([1]);
+  // });
+  // it ("returns tail end of []", () => {
+  //   tail([]);
+  // });
+  it ("returns empty for single.length", () => {
+    const single = [1];
+    const singleTail = tail(single);
+    const empty = [];
+    const emptyTail = tail(empty);
+    // assert.strictEqual(singleTail.length, emptyTail.length);
+    assert.deepEqual(singleTail, emptyTail);
+  });
+});
