bdbc7323a28884c914adbac54e22237d0d5691f1
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-29 01:45:24 +0000

    Made us of Mocha and Chai to create format and test code

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5); //5 is the head of this array
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //"Hello" is the head of this array
-assertEqual(head([]),undefined); //empty array yields undefined as its head
-assertEqual(head([1]),2); //single element array still yields that one element as its head
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns '1' as value of single element array [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+});
