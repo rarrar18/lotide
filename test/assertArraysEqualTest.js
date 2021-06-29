7e7581ff2d9ef0431fc42c05afb7d1900281c303
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-29 05:12:20 +0000

    Modified test functions and .json file

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,8 +1,17 @@
 const assertArraysEqual = require('../assertArraysEqual.js')
+// const assert = require('chai').assert;
 
-// TEST CODE for eqArrays()
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+describe("#assertArraysEqual", () => {
+  it("returns true for matching [1, 2, 3] with [1, 2, 3]", () => {
+    assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+  });
+  it("returns false for matching [1, 2, 3] with [3, 2, 1]", () => {
+    assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+  });
+  it("returns true for matching ['1', '2', '3'] with ['1', '2', '3']", () => {
+    assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+  });
+  it("returns false for matching ['1', '2', '3'] with ['1', '2', 3]", () => {
+    assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+  });
+});
--- test/withoutTest.js
+++ test/withoutTest.js
@@ -5,3 +5,5 @@ without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// TEST CODE for assertEqual() and assertArraysEqual()
+// assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true); // => should PASS
