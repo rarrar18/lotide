ea89a39260c54d16182b58a2ece2e4f14d75383a
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-29 03:51:31 +0000

    Modified index.js to use property value shorthands

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
@@ -10,27 +7,30 @@ const eqObjects = require('./eqObjects');
 const eqArrays = require('./eqArrays');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
+const head   = require('./head');
 const letterPositions = require('./letterPositions');
 const map = require('./map');
+const middle = require('./middle');
+const tail   = require('./tail');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  head:   head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
